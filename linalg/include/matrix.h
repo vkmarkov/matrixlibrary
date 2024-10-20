3fb4418d897da03943d5d89e80a5f64c2955c87f
Марков Виктор Константинович <vkmarkov@edu.hse.ru>
2024-10-20 22:21:28 +0300

    Assignment, Index operators

--- linalg/include/matrix.h
+++ linalg/include/matrix.h
@@ -7,9 +7,10 @@ namespace linalg {
 	class Matrix {
 	public: //Constructors
 		Matrix() noexcept {}
-		Matrix(size_t rows);
-		Matrix(size_t rows, size_t columns);
-		Matrix(const Matrix& m);
+		Matrix(size_t rows) noexcept;
+		Matrix(size_t rows, size_t columns) noexcept;
+		Matrix(const Matrix& m) noexcept;
+		Matrix(Matrix&& m) noexcept;
 
 		//Destructor
 		~Matrix() { delete[] m_ptr; }
@@ -19,13 +20,20 @@ namespace linalg {
 		size_t columns() const noexcept { return m_columns; }
 		bool empty() const noexcept { return (m_rows == 0); }
 		void reshape(int new_rows, int new_columns);
+		double& Matrix::operator ()(size_t row, size_t column);
+		const double& Matrix::operator ()(size_t row, size_t column) const;
 
-	private:
+	public: //Assignment operators
+		Matrix& operator=(Matrix m);
+
+		friend std::ostream& operator<<(std::ostream&, const Matrix&);
+
+	private: //Fields
 		double* m_ptr = nullptr;
 		size_t m_rows = 0;
 		size_t m_columns = 0;
 	};
 
 
-
+	//std osrteam operator<<(std::ostream&, const Matrix&);
 }
--- linalg/source/matrix.cpp
+++ linalg/source/matrix.cpp
@@ -4,21 +4,18 @@
 namespace linalg {
 
 	// Constructors
-	Matrix::Matrix(size_t rows) {
-		if (rows <= 0)
-			throw std::invalid_argument("The number of rows of the matrix must be positive");
+	Matrix::Matrix(size_t rows) noexcept {
 		m_rows = rows;
 		m_ptr = new double[rows];
 	}
 
-	Matrix::Matrix(size_t rows, size_t columns) {
-		if (rows <= 0 || columns <= 0) throw std::invalid_argument("The number of rows and columns of the matrix must be positive");
+	Matrix::Matrix(size_t rows, size_t columns) noexcept {
 		m_rows = rows;
 		m_columns = columns;
 		m_ptr = new double[rows * columns];
 	}
 
-	Matrix::Matrix(const Matrix& m) {
+	Matrix::Matrix(const Matrix& m) noexcept {
 		m_rows = m.m_rows;
 		m_columns = m.m_columns;
 		m_ptr = new double[m_rows * m_columns];
@@ -27,17 +24,50 @@ namespace linalg {
 		}
 	}
 
-	//Methods
-	void linalg::Matrix::reshape(int rows, int columns) {
-		if (empty()) throw std::runtime_error("Matrix is empty");
-		if (rows <= 0 || columns <= 0) throw std::runtime_error("Invalid value");
-		if (rows * columns != m_rows * m_columns) {
-			throw std::runtime_error("Number of elements in reshaped matrix must be the same");
-		}
-		m_rows = rows;
-		m_columns = columns;
+	Matrix::Matrix(Matrix&& m) noexcept {
+		std::swap(m_ptr, m.m_ptr);
+		std::swap(m_rows, m.m_rows);
+		std::swap(m_columns, m.m_columns);
 	}
 
 
+	//Methods
+	void linalg::Matrix::reshape(int rows, int columns) {
+		if (rows * columns != m_rows * m_columns) 
+			throw std::runtime_error("Number of elements in reshaped matrix must be the same");
+		m_rows = rows;
+		m_columns = columns;
+	}
+	 
+	//Operators
+
+	Matrix& Matrix::operator=(Matrix other) { //Assignment operator
+		std::swap(*this, other);
+		return *this;
+	}
+
+	std::ostream& operator<<(std::ostream& out, const Matrix& m) { //Output operator
+		for (size_t i = 0; i < m.m_rows; ++i) {
+			for (size_t j = 0; j < m.m_columns; ++j) {
+				out << m(i, j) << ' ';
+			}
+			out << '\n';
+		}
+		return out;
+	}
 	
+	double& Matrix::operator ()(size_t row, size_t column) { // Index operator for non-constant with 2 parameter
+		if (row >= m_rows || column >= m_columns) {
+			throw std::out_of_range("Index out of range");
+		}
+		return m_ptr[row * m_columns + column];
+	}
+
+	const double& Matrix::operator ()(size_t row, size_t column) const { //for constant with 2 parameter 
+		if (row >= m_rows || column >= m_columns) {
+			throw std::out_of_range("Index out of range");
+		}
+		return m_ptr[row * m_columns + column];
+	}
+
 }
